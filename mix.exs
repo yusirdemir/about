55f007f477d0c05b51c6782b7f2a058163f850a7
Yusuf Demir <yusufdemir286634@gmail.com>
2022-10-08 18:58:39 +0300

    added necessary packages

--- mix.exs
+++ mix.exs
@@ -41,7 +41,9 @@ defmodule About.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:crawly, "~> 0.14.0"},
+      {:floki, "~> 0.33.0"}
     ]
   end
 
